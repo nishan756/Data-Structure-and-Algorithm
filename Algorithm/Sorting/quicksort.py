33d0721c7c1ca15254f6d69d143a75636e71aa53
Nishan <aanishan339@gmail.com>
2025-10-18 19:36:50 +0600

    Quicksort algorithm implmented

---
+++ Algorithm/Sorting/quicksort.py
@@ -0,0 +1,26 @@
+from typing import List
+def Partition(array:List , low , high):
+    i = low
+    j = high
+    pivot = array[low]
+    while i < j:
+        while array[i] <= pivot:
+            i += 1
+        while array[j] > pivot:
+            j -= 1
+        if i < j:
+            array[i] , array[j] = array[j] , array[i]
+    
+    array[low] , array[j] = array[j] , array[low]
+    return j
+
+def QuickSort(array:List , low , high):
+    if low < high:
+        p = Partition(array , low , high)
+        QuickSort(array , low , p - 1)
+        QuickSort(array , p + 1 , high)
+    
+    return array
+
+array = [10 , 15 , 1 , 2 , 9 , 16 , 11]
+print(QuickSort(array , 0 , len(array) - 1))
